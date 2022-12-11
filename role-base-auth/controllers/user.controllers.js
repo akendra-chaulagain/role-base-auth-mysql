0899f93551867d2eec0b3c4bc41117eaa6c0f220
Akendra Chaulagain <yakendrachaulagain27@gmail.com>
2022-12-11 20:47:32 +0545

    chang

--- role-base-auth/controllers/user.controllers.js
+++ role-base-auth/controllers/user.controllers.js
@@ -2,9 +2,10 @@ const db = require("../connection/db");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { roles } = require("../utils/constant");
+// changeslmlfml;g,mg;,g
 
 const deleteUser = async (req,res)=>{
-    
+
 
 
 
